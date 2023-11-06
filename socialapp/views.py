33a33f78f746b3da314807ad0a02999f7f42a4f6
JohanPlAr <johan.arkert@gmail.com>
2023-11-06 09:18:56 +0100

    Add base and index templates, urls.py, style.css and views.py

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from. models import Post
 
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status='published')
+    template_name = 'index.html'
+    paginate_by = 6
+    
 # Create your views here.
